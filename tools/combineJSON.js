5d22e89eb30bd53c5214727f17aa97abc93cd71a
David Lannoye <david@lannoye.net>
2014-03-08 11:49:11 -0600

    Adding git ignore and start of the node app

---
+++ tools/combineJSON.js
@@ -0,0 +1,35 @@
+// Combine the JSONs files output by the scrape calls in to one file.
+var fs = require('fs');
+var moment = require('moment');
+
+var currentDate = moment('March 8, 2010');
+
+var logs = {DispatchLog: []};
+
+var getPathForDate = function (date) {
+	return './json/' + date.format('MM-DD-YYYY') + '.json';
+};
+
+while(currentDate.isBefore('March 8, 2014')) {
+	var jsonString = fs.readFileSync(getPathForDate(currentDate), {encoding: 'utf-8'});
+
+	var data = JSON.parse(jsonString);
+
+	logs.DispatchLog = logs.DispatchLog.concat(data.DispatchLog);
+	
+	currentDate.add('d', 1);
+}
+
+console.log(logs.DispatchLog.length);
+
+
+
+//fs.writeFile('./json/combined.json', JSON.stringify(logs), function(err) {
+//	if (err) throw err;
+
+//	console.log('Combined DispatchLog saved');
+
+//});
+
+
+
