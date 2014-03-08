3b2910bdf99e25544e8eeb953793bcf12966a1d2
David Lannoye <david@lannoye.net>
2014-03-08 12:04:40 -0600

    Moving app to port 8080

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(80, "0.0.0.0");
+}).listen(8080, "0.0.0.0");
+console.log('Server running at http://0.0.0.0:8080/');
