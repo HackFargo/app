25892a61fd1212aec5607955627ac98536790897
blaine booher <blaine@cliftonlabs.com>
2014-03-09 13:19:30 -0500

    added routine for updating geo data based on geofudge

--- tools/dbtool.py
+++ tools/dbtool.py
@@ -3,18 +3,104 @@ import os
 import glob
 import simplejson
 from datetime import datetime
+from geofudge import geofudge
 
+# Add useful helper to sqlite interface
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 conn = sqlite3.connect('hackfargo.db')
+conn.row_factory = sqlite3.Row
 c = conn.cursor()
+
+
+def update_record(CFSID, lat, lon):
+    query = 'update DispatchLogs set Lat=?, Long=? where CFSID=?'
+    c.execute(query, (lat, lon, CFSID))
 
 
 def update_geo():
     query = '''select * from DispatchLogs'''
     c.execute(query)
     rows = c.fetchall()
+    success = 0
+    total = len(rows)
     for r in rows:
-        print r
-        break
+        try:
+            b = r['Block']
+            rn = r['StreetName']
+            rt = r['StreetType']
+            su = r['StreetSuffix']
+
+            if len(su) >= 2:
+                su = su[0]
+            if (not su in ['S', 'N']):
+                # Skip it
+                raise ValueError
+
+            if (rt == ''):
+                # '13 AVE'
+                print b, rn, rt, su
+                if rn.upper() == 'BROADWAY':
+                    rt = 'ST'
+                    print '   Broadway Fixed'
+                else:
+                    tokens = r['StreetName'].split(" ")
+                    if (len(tokens) == 1):
+                        # I94
+                        print '   ====> invalid token', tokens
+                        raise
+                    else:
+                        rn = tokens[0]
+                        rt = tokens[1]
+                        print ' ', rn, rt, ' fixed with tokens'
+            # 16 1/2 street
+            if ('1/2' in rn):
+                tokens = rn.split(" ")
+                rn = float(tokens[0]) + .5
+            block = int(b)
+            roadNum = float(rn)
+            roadType = str(rt.upper())
+            suffix = str(su.upper())
+            if (not rt.upper() in ['ST', 'AVE']):
+                # don't parse lanes, cir, etc.
+                raise ValueError
+        except ValueError:
+            print r['Block'], r['StreetName'].upper(), '... trying to fix'
+            if r['StreetName'].upper() == 'UNIVERSITY':
+                roadNum = 13
+                roadType = "ST"
+                suffix = "N"
+                print '   University fixed'
+            elif r['StreetName'].upper() == 'BROADWAY':
+                roadNum = 6
+                roadType = "ST"
+                suffix = "N"
+                print '   Broadway Fixed (mapped)'
+            elif r['StreetName'].upper() == 'MAIN':
+                roadNum = 0
+                roadType = "AVE"
+                suffix = "N"
+                print '   MAIN Fixed'
+            elif r['StreetName'].upper() == 'NORTHERN PACIFIC':
+                roadNum = .5
+                roadType = "AVE"
+                suffix = "N"
+                print '   NP Ave Fixed'
+            else:
+                print '   invalid name:', b, rn, rt, su
+                continue
+
+        lat, lon = geofudge(int(block), float(roadNum), str(roadType), str(suffix))
+        success += 1
+
+        update_record(r['CFSID'], lat, lon)
+    conn.commit()
+    print success, 'successful lookups of ', total, ' total rows'
 
 
 def dbinit():
@@ -116,8 +202,8 @@ def populate(folder='json/'):
 # CREATE UNIQUE INDEX index_name ON DispatchLogs (column_name)
 
 if __name__ == "__main__":
-    dbinit()
-    populate()
+    # dbinit()
+    # populate()
 
     update_geo()
     conn.close()
