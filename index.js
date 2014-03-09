587c22cf2b9ef0d79a8983636d4839b076e33a19
David Lannoye <david@lannoye.net>
2014-03-08 21:24:49 -0600

    Adding inital server

--- index.js
+++ index.js
@@ -2,46 +2,61 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var sqlite3 = require('sqlite3');
 var moment = require('moment');
 
-var getPathForDate = function (date) {
-	return './tools/json/' + date.format('MM-DD-YYYY') + '.json';
-};
+var defaultDate = moment('March 7, 2014')
+var db = new sqlite3.Database("./tools/hackfargo.db");
 
-app.get('/calls', function(req, res){
+var processRequest = function(req, res) {
+	var startDate = defaultDate, endDate;
 
-	var startDate = moment('March 8, 2010'), endDate;
-	
 	if (req.query.start) {
 		startDate = moment(req.query.start);
 	}
 
 	if (req.query.end) {
 		endDate = moment(req.query.end);
+	} else {
+		endDate = startDate;
 	}
-
-	fs.readFile(getPathForDate(date), {encoding: 'utf-8'}, function(err, data){
-		var callsForDay = JSON.parse(data);
-		res.json(callsForDay);
-	});
 	
-});
+	// Set the end date to the end of the day.
+	endDate.endOf('day')
+
+	var descriptionColumnName = 'NatureOfCall';
+	db.all("SELECT * FROM DispatchLogs WHERE DateVal BETWEEN ? AND ? LIMIT 10000", startDate.unix(), endDate.unix(), function(err, rows) {
+	 
+		if (err) {
+			res.send(500, { error: 'something blew up' });
+		}
+
+		if (rows)
+		{ 
+			var dataSet = []
+			rows.forEach(function (row) {
+				dataSet.push({
+					DataSetID: 'DispatchLogs',
+					Lat: row.Lat,
+					Long: row.Long,
+					Date: row.DateVal,
+					Description: row[descriptionColumnName],
+					Meta: row,
+				})
+			});
+			res.json(dataSet);
+		}
+	});
+};
 
 app.get('/calls', function(req, res){
-
-	var date = moment('March 8, 2010');
-	if (req.query.start) {
-		date = moment(req.query.start);
-	}
-
-
-	fs.readFile(getPathForDate(date), {encoding: 'utf-8'}, function(err, data){
-		var calls = JSON.parse(data);
-		res.json(calls);
-	});
-	
+	processRequest(req, res);
 });
+	
 
+app.get('/calls/type/:id', function(req, res) {
+  processRequest(req, res);
+});
 
 app.listen(8181);
 console.log('Server running at http://0.0.0.0:8181/');
