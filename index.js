4882ddb0e53a922d226a73229f0250461cd23b6a
David Lannoye <david@lannoye.net>
2014-03-08 22:36:34 -0600

    Adding headers to allow cross orgin request

--- index.js
+++ index.js
@@ -75,6 +75,12 @@ var processRequest = function(req, res, descriptionColumnName, descriptionFilter
 	});
 };
 
+app.all('/', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+ });
+
 app.get('/calls', function(req, res){
 	processRequest(req, res, 'NatureOfCall');
 });
