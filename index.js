e00c5c2797e5c448b7def000369ee8c2400feb71
David Lannoye <david@lannoye.net>
2014-04-06 22:43:59 -0500

    Adding ability to specify max/min Lats and Longs. This alllows for 'geofenced' queries

--- index.js
+++ index.js
@@ -21,28 +21,28 @@ var fakeLong = function() {
 	return longMin + (longMax - longMin) * Math.random();
 };
 
-// tableName and descriptionColumn name should never com from user data since 
+// tableName and descriptionColumn name should never com from user data since
 // that would open us up to SQL injection attacks.
 var processRequestRandom = function(req, res, tableName, descriptionColumnName, descriptionFilter) {
 
 	if (!descriptionColumnName) {
 		res.send(500, { error: 'something blew up' });
-		return;	
+		return;
 	}
 
 	// Initialize an empty data set.
 	var dataSet = [];
-	
-	db.each("SELECT * FROM " + tableName + " WHERE GeoLookupType=1 order by RANDOM() LIMIT 100", 
+
+	db.each("SELECT * FROM " + tableName + " WHERE GeoLookupType=1 order by RANDOM() LIMIT 100",
 		function(err, row) {  // Row Handler, called once per row.
-	 
+
 		if (err) {
 			res.send(500, { error: 'something blew up' });
 			return;
 		}
 
 		if (row)
-		{ 
+		{
 			dataSet.push({
 				DataSetID: tableName,
 				Lat: row.Lat ? row.Lat : fakeLat(),
@@ -63,20 +63,42 @@ var processRequestRandom = function(req, res, tableName, descriptionColumnName, 
 	});
 };
 
-// tableName and descriptionColumn name should never com from user data since 
+// tableName and descriptionColumn name should never com from user data since
 // that would open us up to SQL injection attacks.
 var processRequest = function(req, res, tableName, descriptionColumnName, descriptionFilter) {
-	var startDate = moment().subtract('days', 15), 
+	var startDate = moment().subtract('days', 15),
 		endDate;
 
 	if (!descriptionColumnName) {
 		res.send(500, { error: 'something blew up' });
-		return;	
+		return;
 	}
 
 	if (!descriptionFilter) {
 		descriptionFilter = '';
-	}	
+	}
+
+	var maxLat = 90,
+			minLat = -90,
+			maxLong = 180,
+			minLong = -180;
+
+	if (req.query.maxLat) {
+		maxLat = req.query.maxLat;
+	}
+
+	if (req.query.minLat) {
+		minLat = req.query.minLat;
+	}
+
+	if (req.query.maxLong) {
+		maxLong = req.query.maxLong;
+	}
+
+	if (req.query.minLong) {
+		minLong = req.query.minLong;
+	}
+
 
 	// Retrieve the start and end date or create one if needed.
 	if (req.query.start && moment(req.query.start).isValid()) {
@@ -90,7 +112,7 @@ var processRequest = function(req, res, tableName, descriptionColumnName, descri
 	} else {
 		endDate = moment(startDate).add('days', 15)
 	}
-	
+
 	// Set the end date to the end of the day.
 	if (endDate){
 		endDate.endOf('day');
@@ -106,22 +128,26 @@ var processRequest = function(req, res, tableName, descriptionColumnName, descri
 	} else {
 		orderBy = 'DateVal';
 	}
-	
-	db.each("SELECT * FROM " + tableName + " WHERE " + descriptionColumnName + " LIKE ? AND DateVal BETWEEN ? AND ? ORDER BY ? DESC LIMIT ?", 
-			'%' + descriptionFilter + '%', 
-			startDate.unix(), 
-			endDate.unix(), 
+
+	db.each("SELECT * FROM " + tableName + " WHERE " + descriptionColumnName + " LIKE ? AND DateVal BETWEEN ? AND ? AND Lat BETWEEN ? AND ? AND Long BETWEEN ? AND ? ORDER BY ? DESC LIMIT ?",
+			'%' + descriptionFilter + '%',
+			startDate.unix(),
+			endDate.unix(),
+			minLat,
+			maxLat,
+			minLong,
+			maxLong,
 			orderBy,
 			limit,
 		function(err, row) {  // Row Handler, called once per row.
-	 
+
 		if (err) {
 			res.send(500, { error: 'something blew up' });
 			return;
 		}
 
 		if (row)
-		{ 
+		{
 			dataSet.push({
 				DataSetID: tableName,
 				Lat: row.Lat ? row.Lat : fakeLat(),
@@ -169,7 +195,7 @@ app.get('/permits', function(req, res){
 	addHeaders(res);
 	processRequest(req, res, 'BuildingPermits','PermitType');
 });
-	
+
 
 app.get('/permits/type/:typeFilter', function(req, res) {
 	addHeaders(res);
