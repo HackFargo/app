655999107c5f2dafc5d4d486cf5664af984744c7
blaine booher <blaine@cliftonlabs.com>
2014-06-01 16:52:50 -0500

    change default port, added root handler

--- api/geocoder/geocode.go
+++ api/geocoder/geocode.go
@@ -1,5 +1,28 @@
-// api for returning geocoded strings from the postgis-enabled tiger database
 /*
+
+An HTTP API for a working postgis installation
+Author: Blaine Booher (for HackFargo)
+
+Current Status:
+   * /geocode/<query:string> -- fully functional
+
+   		To use the api endpoint, simply set up config.json with your
+   		postgres parameters (in current directory), and run this
+   		application. Then visit "http://localhost:8282/geocode/Fargo, ND"
+   		which will return a json geocoded packet like:
+   		{'lon': -96.88020266719587425541, 'lat': 46.71740545406462530309, 'rating': 100}
+
+   		Data format subject to change, but likely only by adding new fields.
+
+   		You can put any parsable string in there -- postgis handles the rest.
+
+   		ex: "Fargo, ND", "Fargo, North Dakota 58103", "400 NP Ave, Fargo ND"
+
+   		Production (running on http://gis.hackfargo.co) has minnesota and
+   		north dakota tiger data loaded (but no other states), which should
+   		cut down on abuse.
+
+
 time psql -d hackfargo_gis -c "
 SELECT g.rating, ST_X(g.geomout) As lon, ST_Y(g.geomout) As lat, (addy).address As stno, (addy).streetname As street,
 (addy).streettypeabbrev As styp, (addy).location As city, (addy).stateabbrev As st,(addy).zip
@@ -105,6 +128,10 @@ func http_geocoder(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "{'lon': %.20f, 'lat': %.20f, 'rating': %d}", gc.lon, gc.lat, gc.rating)
 }
 
+func http_root(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there! Check out http://hackfargo.co")
+}
+
 func main() {
 	//lon, lat := geocode(db, "Fargo, ND")
 	r := geocode(db, "Fargo, ND")
@@ -112,5 +139,6 @@ func main() {
 
 	fmt.Println("Server listening on :9999")
 	http.HandleFunc("/geocode/", http_geocoder)
-	http.ListenAndServe(":9999", nil)
+	http.HandleFunc("/", http_root)
+	http.ListenAndServe(":8282", nil)
 }
