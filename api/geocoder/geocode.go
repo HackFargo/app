398978a04236027b90ea8f1f420ee526261632ad
blaine booher <blaine@cliftonlabs.com>
2014-06-02 00:43:58 -0500

    tweaking string format

--- api/geocoder/geocode.go
+++ api/geocoder/geocode.go
@@ -130,7 +130,7 @@ func http_geocoder(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "[")
 	for i := 0; i < len(gc); i++ {
 		// stno | street | styp |   city    | st |  zip
-		fmt.Fprintf(w, "{'lon': %.20f, 'lat': %.20f, 'rating': %d, 'stno': '%s', 'street': '%s', 'styp': '%s', city': '%s', 'state': '%s', 'zip': %'s'},", gc[i].lon, gc[i].lat, gc[i].rating, gc[i].stno.String, gc[i].street.String, gc[i].styp.String, gc[i].city.String, gc[i].st.String, gc[i].zip.String)
+		fmt.Fprintf(w, "{'lon': %.20f, 'lat': %.20f, 'rating': %d, 'stno': '%s', 'street': '%s', 'styp': '%s', city': '%s', 'state': '%s', 'zip': '%s'},", gc[i].lon, gc[i].lat, gc[i].rating, gc[i].stno.String, gc[i].street.String, gc[i].styp.String, gc[i].city.String, gc[i].st.String, gc[i].zip.String)
 	}
 	fmt.Fprintf(w, "]")
 }
